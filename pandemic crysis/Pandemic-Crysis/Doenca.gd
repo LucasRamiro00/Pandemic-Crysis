63c4cc4421321de09ead58f042eb1e61cee5a49f
LucasRamiro00 <lucasrunifil@edu.unifil.br>
2026-08-18 14:20:48 -0300

    Add files via upload

---
+++ pandemic crysis/Pandemic-Crysis/Doenca.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name Doenca
+
+@export var nome: String
+@export var sintomas: Array[String]
+@export var recompensa_financeira: int
+@export var semana_de_aparicao: int
---
+++ pandemic crysis/Pandemic-Crysis/resources/doencas/testes.gd
@@ -0,0 +1,30 @@
+extends Node
+
+func _ready() -> void:
+	print("--- INICIANDO SIMULACAO PANDEMIC CRYSIS ---")
+	print("Dia Inicial: ", TimeManager.dia_atual, " | Saldo Inicial: ", GameManager.caixa_hospital)
+	
+	# Pegando a primeira doença do banco (a Gripe que você criou)
+	var doenca_teste = GameManager.banco_de_doencas[0]
+	
+	print("\n--- ATENDIMENTOS ---")
+	# Simulando que o jogador atendeu 12 pacientes perfeitamente
+	for i in range(12):
+		var acertou = GameManager.processar_diagnostico(doenca_teste, doenca_teste)
+	
+	print("Atendimentos feitos. Novo saldo: ", GameManager.caixa_hospital)
+	
+	print("\n--- FECHAMENTO DO DIA ---")
+	GameManager.realizar_fechamento_de_caixa()
+	print("Saldo após pagar as contas do hospital: ", GameManager.caixa_hospital)
+	
+	print("\n--- GESTÃO E UPGRADES ---")
+	print("Leitos antes: ", GameManager.limite_de_leitos)
+	var comprou = GameManager.comprar_upgrade("ampliar_leitos", 300)
+	if comprou:
+		print("Upgrade comprado! Leitos agora: ", GameManager.limite_de_leitos)
+		print("Saldo pós-compra: ", GameManager.caixa_hospital)
+		
+	print("\n--- AVANÇANDO O TEMPO ---")
+	TimeManager.avancar_dia()
+	print("Iniciando Dia: ", TimeManager.dia_atual)
